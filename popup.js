0d8f296d9727df5d0bbb6c8012ef8d5a0b50d1cd
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2020-12-17 18:14:12 +0900

    added images

--- popup.js
+++ popup.js
@@ -1,12 +1,14 @@
 const read = document.getElementById('read');
 const readBackwards = document.getElementById('readBackwards');
+const speedUp = document.getElementById('speedUp');
+const speedDown = document.getElementById('speedDown');
 
-chrome.storage.sync.get('color', function(data) {
+chrome.storage.sync.get('color', function (data) {
   // read.style.backgroundColor = data.color;
   // read.setAttribute('value', data.color);
 });
 
-read.onclick = function(element) {
+read.onclick = function (element) {
   const code = `
   var doc = document.querySelector('.kix-page-content-wrapper');
   var synth = window.speechSynthesis;
@@ -14,28 +16,42 @@ read.onclick = function(element) {
   var utter = new SpeechSynthesisUtterance(textContent);
   synth.speak(utter);
   `;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: code});
-    });
-  };
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.executeScript(tabs[0].id, { code: code });
+  });
+};
 
-  readBackwards.onclick = function(element) {
-    const code = `
+readBackwards.onclick = function (element) {
+  const code = `
     var doc = document.querySelector('.kix-page-content-wrapper');
     var synth = window.speechSynthesis;
     var textContent = doc.textContent;
-    var sentences = textContent.split('.')
-    sentences.reverse();
-    console.log(sentences);
-    textContent = sentences.map(ele => ele + '. ').join();
+    textContent = textContent.split('.').reverse().map(ele => ele + '. ').join();
     var utter = new SpeechSynthesisUtterance(textContent);
     synth.speak(utter);
     `;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: code});
-      });
-    };
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.executeScript(tabs[0].id, { code: code });
+  });
+};
+
+speedUp.onclick = function (element) {
+  const code = `
+  synth.cancel();
+   // utter.rate = 2;
+//    synth.resume();
+    `;
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.executeScript(tabs[0].id, { code: code });
+  });
+};
+
+speedDown.onclick = (element) => {
+  const code = `
+  utter.rate = 2;
+    synth.speak(utter);
+    `;
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.executeScript(tabs[0].id, { code: code });
+  });
+};
