559c9752a13428ef384bdca5b6c4860640ae5569
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2020-12-19 21:45:47 +0900

    can read backwards with period

--- popup.js
+++ popup.js
@@ -4,17 +4,24 @@ const cancel = document.getElementById('cancel');
 const initial = document.getElementById('initial');
 const playing = document.getElementById('playing');
 
-const readBackwards = document.getElementById('readBackwards');
+const readBackwardsCheckBox = document.getElementById('readBackwards');
 const speed = document.getElementById('speed');
 const speedUp = document.getElementById('speedUp');
 const speedDown = document.getElementById('speedDown');
 
 var speedValue;
+var readBackwards;
 
 chrome.storage.sync.get('speed', (data) => {
-  console.log('data', data.speed);
+  console.log('speed', data.speed);
   speedValue = Number.isNaN(Number.parseFloat(data.speed)) ? 1 : data.speed;
   setSpeedText();
+});
+
+chrome.storage.sync.get('readBackwards', (data) => {
+  console.log('readBackwards', data.readBackwards);
+  readBackwards = data.readBackwards || false;
+  readBackwardsCheckBox.checked = true;
 });
 
 const showInitial = () => {
@@ -28,12 +35,14 @@ const showPlaying = () => {
 };
 
 read.onclick = function (element) {
+  const reverseTextContentCode = `textContent = textContent.split('.').reverse().map(ele => ele + '. ').join();`;
   showPlaying();
   const code = `
   var googleDocument = getGoogleDocument();
   var doc = document.querySelector('.kix-page-content-wrapper');
   var synth = window.speechSynthesis;
   var textContent = googleDocument.selectedText || doc.textContent;
+  ${readBackwards ? reverseTextContentCode : ''}
   var utter = new SpeechSynthesisUtterance(textContent);
   utter.rate = ${speedValue}
   synth.speak(utter);
@@ -88,3 +97,14 @@ const setSpeedText = () => {
 Number.prototype.clamp = function (min, max) {
   return Math.min(Math.max(this, min), max);
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('inside dom content loaded');
+  readBackwardsCheckBox.addEventListener('change', checkBoxChangeHandler);
+});
+
+function checkBoxChangeHandler() {
+  console.log('inside checkbox change handler');
+  readBackwards = readBackwardsCheckBox.checked;
+  chrome.storage.sync.set({ readBackwards: readBackwards }, () => {});
+}
