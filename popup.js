edf5a6dd47cbc51be3178bfdc78d6a3e8f5fea2a
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2020-12-19 09:55:58 +0900

    creating layout

--- popup.js
+++ popup.js
@@ -1,20 +1,48 @@
 const read = document.getElementById('read');
-// const readBackwards = document.getElementById('readBackwards');
-// const speedUp = document.getElementById('speedUp');
-// const speedDown = document.getElementById('speedDown');
+const cancel = document.getElementById('cancel');
 
-chrome.storage.sync.get('color', function (data) {
-  // read.style.backgroundColor = data.color;
-  // read.setAttribute('value', data.color);
+const initial = document.getElementById('initial');
+const playing = document.getElementById('playing');
+
+const readBackwards = document.getElementById('readBackwards');
+const speed = document.getElementById('speed');
+const speedUp = document.getElementById('speedUp');
+const speedDown = document.getElementById('speedDown');
+
+chrome.storage.sync.get('speed', (data) => {
+  const speedValue = Number.isNaN(Number.parseFloat(data)) ? '1.0' : data;
+  speed.textContent = speedValue;
 });
 
+const showInitial = () => {
+  initial.style.display = 'flex';
+  playing.style.display = 'none';
+};
+
+const showPlaying = () => {
+  initial.style.display = 'none';
+  playing.style.display = 'flex';
+};
+
 read.onclick = function (element) {
+  showPlaying();
   const code = `
+  var googleDocument = getGoogleDocument();
   var doc = document.querySelector('.kix-page-content-wrapper');
   var synth = window.speechSynthesis;
-  var textContent = doc.textContent;
+  var textContent = googleDocument.selectedText || doc.textContent;
   var utter = new SpeechSynthesisUtterance(textContent);
   synth.speak(utter);
+  `;
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.executeScript(tabs[0].id, { code: code });
+  });
+};
+
+cancel.onclick = function (element) {
+  showInitial();
+  const code = `
+  synth.cancel();
   `;
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.executeScript(tabs[0].id, { code: code });
